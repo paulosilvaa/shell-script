4950cde439f2773cdb9ca730b76a3fc043c7f48b
Paulo Cesar da Silva <pcsneoh@gmail.com>
2021-04-02 22:32:50 -0300

    Revert "Messing up the code"
    This reverts commit 98e1d36c9c23932cdba24d230d5baa2743c0eba5.

--- analyze.sh
+++ analyze.sh
@@ -79,3 +79,76 @@ pipe(){
 		mkfifo $PIPE
 	fi
 }
+
+listen() {
+if [[ $1 =~ ^(([1-9]|[1-9][0-9]|(1[0-9][0-9]|2[0-5][0-5]))\.){3}([1-9]|[1-9][0-9]|(1[0-9][0-9]|2[0-5][0-5]))$ ]]
+then
+	echo "--- Reading logs ---"
+	log $1 > .$1.log
+
+	if [ ! -f /tmp/last ]
+	then
+        	grep -E '!' $PWD/.$1.log | head -n1 > /tmp/last
+		IFS=$'\n'
+		client=$(grep -A1 -B1 -E 'The distribute fiber is broken' $PWD/$1.log | grep -Eo '[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}|[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}|SlotID: [0-9]{1,2}, PortID: [0-9]{1,2}, ONT ID: [0-9]{1,3}' | sed 'N;s/\n/ /' | sed -r 's/(SlotID: [0-9]{1,2}, PortID: [0-9]{1,2}, ONT ID: [0-9]{1,3}) ([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})/\2 \1/')
+	
+			for i in $client
+			do 
+				slot=$(echo $i | sed -r 's/.*(SlotID: [0-9]{1,2}, PortID: [0-9]{1,2}, ONT ID: [0-9]{1,3})/\1/')
+				onu=$(echo $i | sed -r 's/.*(SlotID: [0-9]{1,2}, PortID: [0-9]{1,2}, ONT ID: [0-9]{1,3})/\1/')
+				parsed=$(echo $onu | sed -r 's/\w+: ([[:digit:]]), \w+: ([[:digit:]]{1,2}), \w+ \w+:/\1 \2/')
+				login $1 > .$1-user.log
+				username=$(grep -A 25 "display ont info 0 $parsed" $PWD/.$1-user.log | grep "Description" | uniq | awk '{ print $3 }' | sed 's/@.*/@/g')
+				time=$(echo $i | sed -r 's/(.*) (SlotID.*)/\1/')
+
+				echo "---------" &> $PIPE
+				echo "INFO" &> $PIPE
+				echo "Username: $username" &> $PIPE
+				echo "ONU: $onu" &> $PIPE
+				echo "Time: $time" &> $PIPE
+				echo &> $PIPE
+			done
+	else
+        	last=$(cat /tmp/last)
+        	first=$(grep -E '!' $PWD/.$1.log| head -n1)
+        	sed -i "/$first/,/$last/!d" $PWD/.$1.log
+        	grep -E '!' $PWD/.$1.log | head -n1 > /tmp/last
+
+		IFS=$'\n'
+		client=$(grep -A1 -B1 -E 'The distribute fiber is broken' $PWD/.$1.log | grep -Eo '[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}|[0-9]{2}-[0-9]{2}-[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}|SlotID: [0-9]{1,2}, PortID: [0-9]{1,2}, ONT ID: [0-9]{1,3}' | sed 'N;s/\n/ /' | sed -r 's/(SlotID: [0-9]{1,2}, PortID: [0-9]{1,2}, ONT ID: [0-9]{1,3}) ([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})/\2 \1/')
+	
+		for i in $client
+		do 
+			slot=$(echo $i | sed -r 's/.*(SlotID: [0-9]{1,2}, PortID: [0-9]{1,2}, ONT ID: [0-9]{1,3})/\1/')
+			onu=$(echo $i | sed -r 's/.*(SlotID: [0-9]{1,2}, PortID: [0-9]{1,2}, ONT ID: [0-9]{1,3})/\1/')
+			parsed=$(echo $onu | sed -r 's/\w+: ([[:digit:]]), \w+: ([[:digit:]]{1,2}), \w+ \w+:/\1 \2/')
+			login $1 > .$1-user.log
+			username=$(grep -A 25 "display ont info 0 $parsed" $PWD/.$1-user.log | grep "Description" | uniq | awk '{ print $3 }' | sed 's/@.*/@/g')
+			time=$(echo $i | sed -r 's/(.*) (SlotID.*)/\1/')
+
+				echo "---------" &> $PIPE
+				echo "INFO" &> $PIPE
+				echo "Username: $username" &> $PIPE
+				echo "ONU: $onu" &> $PIPE
+				echo "Time: $time" &> $PIPE
+				echo &> $PIPE
+		done
+
+	fi
+else
+	echo "IP syntax is wrong... Please validate the IP address!"
+fi
+}
+
+case $1 in
+	-i)
+		shift
+		pipe
+		trap "kill_process" 2
+		while true;do listen $1 ; sleep 3;done&
+		tail -f $PIPE
+		;;
+	*)
+		usage
+		;;
+esac
